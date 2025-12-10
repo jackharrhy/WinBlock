411ab95055ad78d92cc16079a7198020dc298b26
Jack Harrhy <me@jackharrhy.com>
2025-12-10 00:47:19 -0330

    winblock

---
+++ unit1.pas
@@ -0,0 +1,38 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.Button1Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
