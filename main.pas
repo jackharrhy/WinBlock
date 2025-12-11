31c13293f57b5018541b0d5268cd1adeedd3c2b3
Jack Harrhy <me@jackharrhy.com>
2025-12-11 01:58:08 -0330

    oh it grids

--- main.pas
+++ main.pas
@@ -17,7 +17,15 @@ type
     Panel3: TPanel;
     procedure FormCreate(Sender: TObject);
   private
+    SelectedImage: TPicture;
+    GridState: array[0..15, 0..15] of Integer;
+    GridImages: array[0..15, 0..15] of TImage;
+    PaletteImages: TList;
+    SelectedShape: TShape;
     procedure LoadToPanel(const Dir: string);
+    procedure CreateGrid;
+    procedure PaletteImageClick(Sender: TObject);
+    procedure GridImageClick(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
   public
 
   end;
@@ -59,6 +67,8 @@ begin
         Img.Stretch := True;
         Img.Width := 32;
         Img.Height := 32;
+        Img.OnClick := @PaletteImageClick;
+        PaletteImages.Add(Img);
         
         Inc(Col);
         if Col >= ImagesPerRow then
@@ -75,9 +85,98 @@ begin
   FindClose(SR);
 end;
 
+procedure TMainForm.CreateGrid;
+var
+  Col, Row: Integer;
+  CellSize: Integer;
+  Img: TImage;
+begin
+  CellSize := Panel3.Width div 16;
+  
+  for Row := 0 to 15 do
+    for Col := 0 to 15 do
+    begin
+      Img := TImage.Create(Panel3);
+      Img.Parent := Panel3;
+      Img.Left := Col * CellSize;
+      Img.Top := Row * CellSize;
+      Img.Width := CellSize;
+      Img.Height := CellSize;
+      Img.Stretch := True;
+      Img.Picture.Bitmap := nil;
+      GridImages[Col, Row] := Img;
+      GridState[Col, Row] := -1;
+      
+      Img.OnMouseDown := @GridImageClick;
+    end;
+end;
+
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
+  PaletteImages := TList.Create;
+  SelectedImage := nil;
+  SelectedShape := nil;
   LoadToPanel('.\bin');
+  CreateGrid;
+end;
+
+procedure TMainForm.PaletteImageClick(Sender: TObject);
+var
+  Img: TImage;
+begin
+  if Sender is TImage then
+  begin
+    SelectedImage := TPicture.Create;
+    SelectedImage.Assign(TImage(Sender).Picture);
+    
+    if Assigned(SelectedShape) then
+      SelectedShape.Free;
+    
+    SelectedShape := TShape.Create(Panel1);
+    SelectedShape.Parent := Panel1;
+    SelectedShape.Shape := stRectangle;
+    SelectedShape.Brush.Style := bsClear;
+    SelectedShape.Pen.Color := clBlue;
+    SelectedShape.Pen.Width := 2;
+    SelectedShape.Left := TImage(Sender).Left - 2;
+    SelectedShape.Top := TImage(Sender).Top - 2;
+    SelectedShape.Width := TImage(Sender).Width + 4;
+    SelectedShape.Height := TImage(Sender).Height + 4;
+    SelectedShape.BringToFront;
+    TImage(Sender).BringToFront;
+  end;
+end;
+
+procedure TMainForm.GridImageClick(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+var
+  Col, Row: Integer;
+  CellSize: Integer;
+  Img: TImage;
+begin
+  if Sender is TImage then
+  begin
+    Img := TImage(Sender);
+    CellSize := Panel3.Width div 16;
+    Col := Img.Left div CellSize;
+    Row := Img.Top div CellSize;
+    
+    if (Col >= 0) and (Col <= 15) and (Row >= 0) and (Row <= 15) then
+    begin
+      if Button = mbLeft then
+      begin
+        if Assigned(SelectedImage) then
+        begin
+          Img.Picture.Assign(SelectedImage);
+          GridState[Col, Row] := 1;
+        end;
+      end
+      else if Button = mbRight then
+      begin
+        Img.Picture.Bitmap := nil;
+        GridState[Col, Row] := -1;
+      end;
+    end;
+  end;
 end;
 
 end.
