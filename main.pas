56da7df61089ed59dcf6fad447f0e4e72569168b
Jack Harrhy <me@jackharrhy.com>
2025-12-13 01:35:28 -0330

    middle click to select

--- main.pas
+++ main.pas
@@ -12,6 +12,7 @@ type
   { TMainForm }
 
   TMainForm = class(TForm)
+    ExportMemo: TMemo;
     Panel1: TPanel;
     Panel2: TPanel;
     Panel3: TPanel;
@@ -24,13 +25,13 @@ type
     PaletteImages: TList;
     PaletteFilenames: TStringList;
     SelectedShape: TShape;
-    ExportMemo: TMemo;
     GridBackground: TImage;
     procedure LoadToPanel(const Dir: string);
     procedure CreateGrid;
-    procedure CreateExportUI;
     procedure PaletteImageClick(Sender: TObject);
     procedure GridImageClick(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+    procedure CopyFromGridCell(Col, Row: Integer);
+    procedure ShowSelectionBorder(PaletteIndex: Integer);
     procedure UpdateExportText;
   public
 
@@ -56,7 +57,7 @@ begin
   ImagesPerRow := Panel1.Width div 32;
   Col := 0;
   Row := 0;
-  
+
   if FindFirst(Dir + PathDelim + '*.*', faAnyFile, SR) = 0 then
   repeat
     if (SR.Attr and faDirectory) = 0 then
@@ -76,14 +77,14 @@ begin
         Img.OnClick := @PaletteImageClick;
         PaletteImages.Add(Img);
         PaletteFilenames.Add(ChangeFileExt(SR.Name, ''));
-        
+
         Inc(Col);
         if Col >= ImagesPerRow then
         begin
           Col := 0;
           Inc(Row);
         end;
-        
+
       except
         Pic.Free;
       end;
@@ -100,7 +101,7 @@ var
   GridBitmap: TBitmap;
 begin
   CellSize := Panel3.Width div 16;
-  
+
   // Create grid background image
   GridBackground := TImage.Create(Panel3);
   GridBackground.Parent := Panel3;
@@ -110,7 +111,7 @@ begin
   GridBackground.Height := Panel3.Height;
   GridBackground.Stretch := False;
   GridBackground.SendToBack;
-  
+
   // Draw grid on background
   GridBitmap := TBitmap.Create;
   GridBitmap.SetSize(Panel3.Width, Panel3.Height);
@@ -118,24 +119,24 @@ begin
   GridBitmap.Canvas.FillRect(0, 0, GridBitmap.Width, GridBitmap.Height);
   GridBitmap.Canvas.Pen.Color := clGray;
   GridBitmap.Canvas.Pen.Width := 1;
-  
+
   // Vertical lines
   for Col := 0 to 16 do
   begin
     GridBitmap.Canvas.MoveTo(Col * CellSize, 0);
     GridBitmap.Canvas.LineTo(Col * CellSize, Panel3.Height);
   end;
-  
+
   // Horizontal lines
   for Row := 0 to 16 do
   begin
     GridBitmap.Canvas.MoveTo(0, Row * CellSize);
     GridBitmap.Canvas.LineTo(Panel3.Width, Row * CellSize);
   end;
-  
+
   GridBackground.Picture.Assign(GridBitmap);
   GridBitmap.Free;
-  
+
   // Create transparent grid cells
   for Row := 0 to 15 do
     for Col := 0 to 15 do
@@ -149,26 +150,12 @@ begin
       Img.Stretch := True;
       Img.Picture.Bitmap := nil;
       Img.Transparent := True;
-      
+
       GridImages[Col, Row] := Img;
       GridState[Col, Row] := -1;
-      
+
       Img.OnMouseDown := @GridImageClick;
     end;
-end;
-
-procedure TMainForm.CreateExportUI;
-begin
-  ExportMemo := TMemo.Create(Panel2);
-  ExportMemo.Parent := Panel2;
-  ExportMemo.Left := 10;
-  ExportMemo.Top := 10;
-  ExportMemo.Width := Panel2.Width - 20;
-  ExportMemo.Height := Panel2.Height - 20;
-  ExportMemo.ScrollBars := ssVertical;
-  ExportMemo.WordWrap := False;
-  ExportMemo.ReadOnly := True;
-  ExportMemo.Text := 'Discord export will appear here...';
 end;
 
 procedure TMainForm.FormCreate(Sender: TObject);
@@ -180,7 +167,33 @@ begin
   SelectedShape := nil;
   LoadToPanel('.\bin');
   CreateGrid;
-  CreateExportUI;
+  UpdateExportText;
+end;
+
+procedure TMainForm.ShowSelectionBorder(PaletteIndex: Integer);
+var
+  PaletteImg: TImage;
+begin
+  if Assigned(SelectedShape) then
+    SelectedShape.Free;
+
+  if (PaletteIndex >= 0) and (PaletteIndex < PaletteImages.Count) then
+  begin
+    PaletteImg := TImage(PaletteImages[PaletteIndex]);
+
+    SelectedShape := TShape.Create(Panel1);
+    SelectedShape.Parent := Panel1;
+    SelectedShape.Shape := stRectangle;
+    SelectedShape.Brush.Style := bsClear;
+    SelectedShape.Pen.Color := clBlue;
+    SelectedShape.Pen.Width := 2;
+    SelectedShape.Left := PaletteImg.Left - 2;
+    SelectedShape.Top := PaletteImg.Top - 2;
+    SelectedShape.Width := PaletteImg.Width + 4;
+    SelectedShape.Height := PaletteImg.Height + 4;
+    SelectedShape.BringToFront;
+    PaletteImg.BringToFront;
+  end;
 end;
 
 procedure TMainForm.PaletteImageClick(Sender: TObject);
@@ -192,10 +205,10 @@ begin
   begin
     if Assigned(SelectedImage) then
       SelectedImage.Free;
-      
+
     SelectedImage := TPicture.Create;
     SelectedImage.Assign(TImage(Sender).Picture);
-    
+
     for I := 0 to PaletteImages.Count - 1 do
     begin
       if TImage(PaletteImages[I]) = Sender then
@@ -204,22 +217,8 @@ begin
         Break;
       end;
     end;
-    
-    if Assigned(SelectedShape) then
-      SelectedShape.Free;
-    
-    SelectedShape := TShape.Create(Panel1);
-    SelectedShape.Parent := Panel1;
-    SelectedShape.Shape := stRectangle;
-    SelectedShape.Brush.Style := bsClear;
-    SelectedShape.Pen.Color := clBlue;
-    SelectedShape.Pen.Width := 2;
-    SelectedShape.Left := TImage(Sender).Left - 2;
-    SelectedShape.Top := TImage(Sender).Top - 2;
-    SelectedShape.Width := TImage(Sender).Width + 4;
-    SelectedShape.Height := TImage(Sender).Height + 4;
-    SelectedShape.BringToFront;
-    TImage(Sender).BringToFront;
+
+    ShowSelectionBorder(SelectedImageIndex);
   end;
 end;
 
@@ -235,7 +234,7 @@ begin
     CellSize := Panel3.Width div 16;
     Col := Img.Left div CellSize;
     Row := Img.Top div CellSize;
-    
+
     if (Col >= 0) and (Col <= 15) and (Row >= 0) and (Row <= 15) then
     begin
       if Button = mbLeft then
@@ -247,13 +246,37 @@ begin
           UpdateExportText;
         end;
       end
-      else if Button = mbRight then
+    else if Button = mbRight then
       begin
         Img.Picture.Bitmap := nil;
         GridState[Col, Row] := -1;
         UpdateExportText;
+      end
+      else if Button = mbMiddle then
+      begin
+        CopyFromGridCell(Col, Row);
       end;
     end;
+  end;
+end;
+
+procedure TMainForm.CopyFromGridCell(Col, Row: Integer);
+var
+  CellValue: Integer;
+begin
+  CellValue := GridState[Col, Row];
+
+  if CellValue >= 0 then
+  begin
+    // Copy the image from the grid cell to selection
+    if Assigned(SelectedImage) then
+      SelectedImage.Free;
+
+    SelectedImage := TPicture.Create;
+    SelectedImage.Assign(GridImages[Col, Row].Picture);
+    SelectedImageIndex := CellValue;
+
+    ShowSelectionBorder(SelectedImageIndex);
   end;
 end;
 
@@ -271,7 +294,7 @@ begin
   MinRow := 15;
   MaxRow := 0;
   HasContent := False;
-  
+
   for Row := 0 to 15 do
   begin
     for Col := 0 to 15 do
@@ -286,15 +309,15 @@ begin
       end;
     end;
   end;
-  
+
   if not HasContent then
   begin
     ExportMemo.Text := 'Discord export will appear here...';
     Exit;
   end;
-  
+
   ExportText := '';
-  
+
   // Export only the shrinkwrapped area
   for Row := MinRow to MaxRow do
   begin
@@ -316,7 +339,7 @@ begin
     if Row < MaxRow then
       ExportText := ExportText + #13#10;
   end;
-  
+
   ExportMemo.Text := ExportText;
 end;
 
